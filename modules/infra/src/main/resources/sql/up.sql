bd8b62d168604e364cd29d894ba268fbf5761221
petitviolet <violethero0820@gmail.com>
2019-05-06 22:45:25 +0900

    modified DB config

--- modules/infra/src/main/resources/sql/up.sql
+++ modules/infra/src/main/resources/sql/up.sql
@@ -1,7 +1,9 @@
-CREATE TABLE `user` (
+CREATE TABLE `sample`.`user` (
   id char(36) not null,
+  email varchar(255) not null,
   name varchar(100) not null,
-  group_id char(36) not null,
+  status char(16) not null,
+  visibility char(16) not null,
   created_at datetime not null default current_timestamp,
   updated_at datetime not null default current_timestamp,
   primary key(id)
